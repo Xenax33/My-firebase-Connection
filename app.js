51e5cb0743188209248e96f155d933e90e005ee6
Xenax33 <99313393+Xenax33@users.noreply.github.com>
2024-10-09 21:48:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const cors = require("cors");
 const bodyParser = require('body-parser');
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 admin.initializeApp({
